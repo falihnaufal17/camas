0d3258535d2a21f2fd75ddc040de6aec9f27e467
Falih Naufal <=>
2024-01-19 07:27:34 +0700

    fix: title

--- views/index.php
+++ views/index.php
@@ -4,7 +4,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Camas <?php echo isset($_GET['page']) ? '| ' . explode('-', $_GET['page'])[1] : '' ?></title>
+  <?php
+    $title = '';
+    $pageExplode = isset($_GET['page']) ? explode('-', $_GET['page']) : [];
+    array_pop($pageExplode);
+
+    foreach ($pageExplode as $item) {
+      $title .= ' '. $item;
+    }
+  ?>
+  <title>Camas <?= $title ? '|' . $title : '' ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/1ba5ca05fe.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../assets/css/theme.css">
